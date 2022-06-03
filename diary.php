c5ac55cccf539a36ee73ea3fdc8208e70a1ed11c
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-06-03 18:31:02 +0100

    a

--- diary.php
+++ diary.php
@@ -155,9 +155,9 @@
                     for($i=0; $i<$row_count; $i++){
                         if(substr($rows[$i]['address'], -3, -1) != "mp4" && substr($rows[$i]['address'], -3, -1) != "avi" && substr($rows[$i]['address'], -3, -1) != "ogg" && substr($rows[$i]['address'], -3, -1) != "mov"){
                             // We can show pictures then
-                            echo '<img src="'.$rows[$i]['address'].'" alt="'.$rows[$i]['address'].'" width="500" height="500"></img>';
+                            echo '<img src="'.$rows[$i]['address'].'" alt="'.$rows[$i]['address'].'" width="320" height="auto"></img>';
                         }else{
-                            echo '<video width="100" height="auto" controls>';
+                            echo '<video width="320" height="auto" controls>';
                             //width="320" height="240"
                             echo '<source src="'.$rows[$i]['address'].'" type="video/mp4" >';
                             echo 'Your browser does not support the video tag.</video>';
