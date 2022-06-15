1b514a14e2c98b1a30f07f18e8013e756eec1f0f
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-06-16 00:17:21 +0100

    aa

--- tellmemap.php
+++ tellmemap.php
@@ -16,16 +16,31 @@
         exit(1);
     }
 
+	echo '<div id="indicator"></div>';
 	echo '<div id="map"></div>';
+	echo '<div id="placeholder" style="height:200px;"></div>';
 	echo '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
 	echo '<script type="text/javascript">';
-	echo " var map = new maplibregl.Map({
-		container: 'map',
-		style: 'https://api.maptiler.com/maps/streets/style.json?key=873s1SijZFFScPeHZHFB',
-		center: [-2.234992488745344, 53.47414393324406],
-		zoom: 17,
-		});
-	</script>";
+	// echo " var map = new maplibregl.Map({
+	// 	container: 'map',
+	// 	style: 'https://api.maptiler.com/maps/streets/style.json?key=873s1SijZFFScPeHZHFB',
+	// 	center: [-2.234992488745344, 53.47414393324406],
+	// 	zoom: 17,
+	// 	});
+	// </script>";
+
+	echo "mapboxgl.accessToken = 'pk.eyJ1IjoiZGludWQxMSIsImEiOiJjbDE1Nzdib3QwaDJ6M2pzZ2p4bGdhZWo2In0.pNx1qRgo7vsmuoVt0R5-nQ';
+			var map = new mapboxgl.Map({
+			container: 'map',
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [-2.230912, 53.465211],
+			zoom: 6
+			});
+			var coordinates = [];
+			var loopTime = 0;
+			var bounds = new mapboxgl.LngLatBounds();
+		";
+	echo "</script>";
 
 
 	try{
@@ -53,12 +68,37 @@
 		}else{
 			for($i = 0; $i < $row_count; $i++){
 				// echo "<script>console.log(".$rows[$i]['longitude'].")</script>";
-				echo "<script>
-				var london = new maplibregl.Marker()
-				 .setLngLat([".$rows[$i]['latitude'].", ".$rows[$i]['longitude']."])
-				 .addTo(map);
-				</script>";
+
+				// echo "<script>
+				// var london = new maplibregl.Marker()
+				//  .setLngLat([".$rows[$i]['latitude'].", ".$rows[$i]['longitude']."])
+				//  .addTo(map);
+				// </script>";
+
+				echo "
+					var marker = new mapboxgl.Marker()
+					.setLngLat([".$rows[$i]['latitude'].", ".$rows[$i]['longitude']."])
+					.setPopup(
+						new mapboxgl.Popup({ offset: 25 }) // add popups
+						.setHTML(
+			       `<h3>".$rows[$i]['date']."</h3><p>at: ".$rows[$i]['time']."</p>`
+					)
+					)
+					.addTo(map);
+					coordinates[loopTime] = [".$rows[$i]['latitude'].", ".$rows[$i]['longitude']."];
+					loopTime ++;
+
+			";
 			}
+
+			echo "// Map zooming to fit all available points
+			if(coordinates.length != 0){
+				for (var i of coordinates){
+					bounds.extend(i);
+				}
+				map.fitBounds(bounds, { padding: 100 });
+			}
+			";
 		}
 
 	}catch(PDOException $e){
@@ -66,6 +106,38 @@
 	}
 
 ?>
+
+<script>
+	var width = document.body.offsetWidth - getElementOffset(document.getElementById("map")).left*2; 
+	document.getElementById("map").style.width = width+"px";
+	document.getElementById("map").style.top = getElementOffset(document.getElementById("indicator")).top + "px";
+	document.getElementById("placeholder").style.height = getElementOffset(document.getElementById("map")).top + 20 + "px";
+	
+	window.addEventListener('resize', function(event) {
+		document.getElementById("map").style.top = getElementOffset(document.getElementById("indicator")).top + "px";
+		var width = document.body.offsetWidth - getElementOffset(document.getElementById("map")).left*2; 
+		document.getElementById("map").style.width = width+"px";
+		document.getElementById("placeholder").style.height = getElementOffset(document.getElementById("map")).top + 20 + "px";
+	}, true);
+	
+	// Fetch an element's left and top according to the 0,0
+	function getElementOffset(element) {
+	let offset = {left: 0, top: 0}
+	let current = element.offsetParent
+
+	offset.left += element.offsetLeft
+	offset.top += element.offsetTop
+
+	while (current !== null) {
+		offset.left += current.offsetLeft
+		offset.top += current.offsetTop
+		current = current.offsetParent
+	}
+	return offset
+	}
+
+</script>
+
 
 <style>
     #map {
