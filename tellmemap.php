a009d1fd1a588107b07bf332274b1099305a29af
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-06-16 00:24:19 +0100

    a

--- tellmemap.php
+++ tellmemap.php
@@ -69,7 +69,7 @@
 			echo'<p class="narrator" style="font-size: x-large; text-align: center;">查询的日期下并没有记录。</p>';
 		}else{
 			for($i = 0; $i < $row_count; $i++){
-				// echo "<script>console.log(".$rows[$i]['longitude'].")</script>";
+				echo "<script>console.log(".$rows[$i]['longitude'].")</script>";
 
 				// echo "<script>
 				// var london = new maplibregl.Marker()
@@ -79,7 +79,7 @@
 
 				echo "
 					var marker = new mapboxgl.Marker()
-					.setLngLat([".$rows[$i]['latitude'].", ".$rows[$i]['longitude']."])
+					.setLngLat([".$rows[$i]['longitude'].", ".$rows[$i]['latitude']."])
 					.setPopup(
 						new mapboxgl.Popup({ offset: 25 }) // add popups
 						.setHTML(
