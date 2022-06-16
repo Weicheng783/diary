1edea5b5e3f3206f2d0cdc661e3471608d3b0663
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-06-16 21:49:11 +0100

    aaa

--- tellmemap.php
+++ tellmemap.php
@@ -163,6 +163,8 @@
 	echo "</script>";
 
 	try{
+		echo '<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">';
+		
 		$user = "weicheng";
 		$password = "awc020826";
 
