c6c9e2d7c7f513ad31cc30ac043d07c43b2002a8
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-06-17 00:17:03 +0100

    aa

--- food.php
+++ food.php
@@ -83,7 +83,7 @@
         $password = "awc020826";
         header("Content-Type: text/html; charset=utf-8");
 
-                echo '<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">';
+                // echo '<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">';
 
                 echo '<p class="narrator" style="font-size: large; text-align: center; color: purple">添加食材表</p>';
                 echo "<div style='display:center; text-align:center;'>";
