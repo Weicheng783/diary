cf21126c6062b49e9edb79ab36b423fdbc22ea65
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-07-21 22:23:18 +0100

    aaa

--- camera_msg_post.php
+++ camera_msg_post.php
@@ -2,7 +2,7 @@
 if(!isset($_REQUEST['msg']) or $_REQUEST['msg'] == ""){
     echo "<script>alert('message cannot be empty.'); location.href='camera_msg.php';</script>";
 }
-$myfile = fopen("/home/weicheng/message.txt", "w") or die("Unable to open file!");
+$myfile = fopen("message.txt", "w") or die("Unable to open file!");
 $txt = $_REQUEST['msg'];
 fwrite($myfile, $txt);
 fclose($myfile);
