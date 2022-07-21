b43c039f50c2bcfb5540a0b0980690e302ef0ad4
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2022-07-21 22:22:39 +0100

    Camera live message

---
+++ camera_msg.php
@@ -0,0 +1,4 @@
+<form action="camera_msg_post.php">
+ 你要对推流发送更新的状态信息：<input type="input"></input>
+ <button type="submit">发送</button>
+</form>
---
+++ camera_msg_post.php
@@ -0,0 +1,9 @@
+<?php
+if(!isset($_REQUEST['msg']) or $_REQUEST['msg'] == ""){
+    echo "<script>alert('message cannot be empty.'); location.href='camera_msg.php';</script>";
+}
+$myfile = fopen("/home/weicheng/message.txt", "w") or die("Unable to open file!");
+$txt = $_REQUEST['msg'];
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
